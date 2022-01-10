87dc14e2ba470e6d0704ede9882fc5a0bcbc58a4
Marcelo Junqueira <marcelo.junqueira@avance.inf.br>
2022-01-10 17:46:40 -0300

    Going up the finished project

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const path = require("path");
+const db = require("./db/connection.js");
+const bodyParser = require("body-parser");
+const Job = require("./models/Job");
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
+const PORT = 3000;
+
+// bodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// handle bars
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, function () {
+  console.log(`O express esta rodando na porta ${PORT}`);
+});
+
+// db connection
+db.authenticate()
+  .then(() => {
+    console.log("Conectou com suceso ao db");
+  })
+  .catch((err) => {
+    console.log("Ocorreu um erro ao conectar", err);
+  });
+
+// routes
+app.get("/", (req, res) => {
+  let search = req.query.job;
+  let query = '%'+search+'%'
+  if (!search) {
+    Job.findAll({ order: [["createdAt", "DESC"]] }).then((jobs) => {
+      res.render("index", {
+        jobs,
+      });
+    })
+    .catch(err => console.log(err));
+  } else {
+    Job.findAll({ 
+        where: {title: {[Op.like]: query}},
+        order: [["createdAt", "DESC"]] }).then((jobs) => {
+        res.render("index", {
+          jobs, search
+        });
+      })
+      .catch(err => console.log(err));
+  }
+});
+
+// jobs routes
+app.use("/jobs", require("./routes/jobs"));
